680198e5529f93c47695af75217d6de13d4562e5
NLCaceres <NLCaceres21@Gmail.com>
2026-04-20 10:54:15 -0700

    Add Bill model to handle the state of the screen and tip calculation
    - Uses BigDecimal to handle the rounding and decimal points allowed in calculation
    - Uses private const value as a default zero amount, "0.00" for when an invalid text is input

---
+++ app/src/main/java/itp341/caceres/nicholas/tipCalculator/Bill.kt
@@ -0,0 +1,28 @@
+package itp341.caceres.nicholas.tipCalculator
+
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+data class Bill(val amount: String, val percent: Float, var split: Int) {
+  companion object {
+    private const val ZERO_BILL = "0.00"
+  }
+  private val roundedPercent
+    get() = percent.toBigDecimal().setScale(2, RoundingMode.HALF_EVEN)
+  val tip: String
+    get() = amount.toBigDecimalOrNull()?.
+      multiply(roundedPercent)?.setScale(2, RoundingMode.HALF_EVEN)?.
+        toString() ?: ZERO_BILL
+  val total: String
+    get() = amount.toBigDecimalOrNull()?.
+      add(BigDecimal(tip))?.setScale(2, RoundingMode.HALF_EVEN)?.
+        toString() ?: ZERO_BILL
+  val perPersonTip: String
+    get() = tip.toBigDecimalOrNull()?.
+      divide(BigDecimal(split), 2, RoundingMode.HALF_EVEN)?.
+        toString() ?: ZERO_BILL
+  val perPersonTotal: String
+    get() = total.toBigDecimalOrNull()?.
+      divide(BigDecimal(split), 2, RoundingMode.HALF_EVEN)?.
+        toString() ?: ZERO_BILL
+}
