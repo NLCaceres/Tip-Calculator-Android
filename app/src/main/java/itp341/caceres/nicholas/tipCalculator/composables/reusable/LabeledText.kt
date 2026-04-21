f2d4ff74740d6f99dfbd08233c8e1b5b03b49aaf
NLCaceres <NLCaceres21@Gmail.com>
2026-04-21 12:08:51 -0700

    Add composable aligning two Text in a Row that also always align vertically
    - In a Column, multiple of these `LabeledText` would align their label's start side and
    text value's starting side by using the `weight` modifier + Row's `fillMaxWidth`

---
+++ app/src/main/java/itp341/caceres/nicholas/tipCalculator/composables/reusable/LabeledText.kt
@@ -0,0 +1,32 @@
+package itp341.caceres.nicholas.tipCalculator.composables.reusable
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun LabeledText(label: String, value: String, modifier: Modifier = Modifier) {
+  Row(modifier.fillMaxWidth()) {
+    Text(label, Modifier.weight(0.3f), fontSize = 18.sp, fontWeight = FontWeight.Bold)
+    Text("$${value}", Modifier.weight(0.7f).padding(start = 16.dp), fontSize = 18.sp)
+  }
+}
+
+@Preview(widthDp = 360, heightDp = 500, showBackground = true)
+@Composable
+fun LabelTextPreview() {
+  Column(Modifier.padding(start = 32.dp)) {
+    LabeledText("Bill Amount", "100.00", modifier = Modifier.padding(top = 20.dp, bottom = 20.dp))
+    LabeledText("Tip Amount", "20.00")
+    LabeledText("Tip", "20.00", modifier = Modifier.padding(top = 20.dp, bottom = 20.dp))
+    LabeledText("Total", "120.00")
+  }
+}
