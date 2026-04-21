a9d7b6dbc1671bb27ff78ad5d0174b5c4bf5f9f6
NLCaceres <NLCaceres21@Gmail.com>
2026-04-21 13:41:13 -0700

    Add Form composable for percentage Slider/SeekBar with weighted widths
    - Using the Row's alignment to vertically center children

---
+++ app/src/main/java/itp341/caceres/nicholas/tipCalculator/composables/reusable/form/LabeledSlider.kt
@@ -0,0 +1,39 @@
+package itp341.caceres.nicholas.tipCalculator.composables.reusable.form
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlin.math.roundToInt
+
+@Composable
+fun LabeledSlider(label: String, percent: Float, onSliderChange: (Float) -> Unit) {
+  Row(Modifier.padding(horizontal = 16.dp, vertical = 10.dp), verticalAlignment = Alignment.CenterVertically) {
+    Text(label, Modifier.weight(0.35f), fontSize = 18.sp, fontWeight = FontWeight.Bold)
+    Text( "${(percent * 100).roundToInt()}%", Modifier.weight(0.15f), fontSize = 18.sp)
+    Slider(percent, onValueChange = { onSliderChange(it) }, modifier = Modifier.weight(0.5f))
+  }
+}
+
+@Preview(widthDp = 360, heightDp = 500, showBackground = true)
+@Composable
+fun LabeledSliderPreview() {
+  var percentage by remember { mutableFloatStateOf(0.15f) }
+  Column {
+    LabeledSlider("Percent", percentage) { percentage = it }
+  }
+  LabeledSlider("Percent", percentage) { percentage = it }
+}
+
