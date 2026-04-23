ad4bd3c6a718452fd570f9532e3253b734ca3bbc
NLCaceres <NLCaceres21@Gmail.com>
2026-04-23 14:27:19 -0700

    Setup MainScreen without per person tip and total section
    - Reading viewModel bill StateFlow, and feeding percent, tip, and total to corresponding
    Slider & Text composables
      - Should probably do the same for the TextField input. Dropdown too?

---
+++ app/src/main/java/itp341/caceres/nicholas/tipCalculator/composables/MainScreen.kt
@@ -0,0 +1,36 @@
+package itp341.caceres.nicholas.tipCalculator.composables
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import itp341.caceres.nicholas.tipCalculator.ViewModelMain
+import itp341.caceres.nicholas.tipCalculator.composables.reusable.LabeledText
+import itp341.caceres.nicholas.tipCalculator.composables.reusable.form.LabeledDropdown
+import itp341.caceres.nicholas.tipCalculator.composables.reusable.form.LabeledSlider
+import itp341.caceres.nicholas.tipCalculator.composables.reusable.form.LabeledTextField
+
+@Composable
+fun MainScreen(viewModel: ViewModelMain) {
+  val bill by viewModel.bill.collectAsState()
+  Column {
+    LabeledTextField("Bill Amount") { viewModel.updateAmount(it) }
+    LabeledSlider("Percent", bill.percent) { viewModel.updatePercent(it) }
+    LabeledText("Tip", bill.tip, Modifier.padding(20.dp, 20.dp, 10.dp, 10.dp))
+    LabeledText("Total",bill.total, Modifier.padding(20.dp, 20.dp, 10.dp, 10.dp))
+    HorizontalDivider(modifier = Modifier.padding(10.dp, 15.dp), thickness = 2.dp, color = Color.DarkGray)
+    LabeledDropdown("Split Bill?", viewModel::updateSplit)
+  }
+}
+
+@Preview(widthDp = 360, heightDp = 500, showBackground = true)
+@Composable
+fun MainScreenPreview() {
+  MainScreen(ViewModelMain())
+}
