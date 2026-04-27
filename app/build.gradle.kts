6baa4a6f388af215bf687e82eb75867be5fd104f
NLCaceres <NLCaceres21@Gmail.com>
2026-04-26 22:31:27 -0700

    Convert `build.gradle` files to Kotlin DSL
    - Updates method calls to include parentheses, strings to double quotes, booleans to add
    "is" prefix, filetree map to Kotlin map builder, and adds type to the top-level "clean" task
    - Also updates small reference in `proguard-rules`

---
+++ app/build.gradle.kts
@@ -0,0 +1,63 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+android {
+    namespace = "itp341.caceres.nicholas.tipCalculator"
+    compileSdkVersion(35)
+
+    defaultConfig {
+        applicationId = "itp341.caceres.nicholas.tipCalculator"
+        minSdkVersion(26)
+        targetSdkVersion(35)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    buildFeatures {
+        compose = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    compilerOptions {
+        languageVersion = KotlinVersion.KOTLIN_2_3
+        jvmTarget = JvmTarget.JVM_17
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    testImplementation("junit:junit:4.13.2")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+
+    implementation("androidx.core:core-ktx:1.16.0")
+
+    val composeBom = platform("androidx.compose:compose-bom:2025.02.00")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.activity:activity-compose:1.10.1")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    androidTestImplementation("androidx.test:core:1.6.1")
+    androidTestImplementation("androidx.test:runner:1.6.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+}
