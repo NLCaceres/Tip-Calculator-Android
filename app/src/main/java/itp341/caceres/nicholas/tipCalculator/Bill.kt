4ed63ce5064f0640b130154406867f3485c3ac04
NLCaceres <NLCaceres21@Gmail.com>
2026-04-27 17:04:18 -0700

    Split tip and total into num getter and price with "$" prefix + tests
    - Number getters used to calculate values with BigDecimal
    - Price with "$" usable by the UI for display
    - Tests now check for the number and the price + "$" prefix

--- app/src/main/java/itp341/caceres/nicholas/tipCalculator/Bill.kt
+++ app/src/main/java/itp341/caceres/nicholas/tipCalculator/Bill.kt
@@ -9,20 +9,27 @@ data class Bill(val amount: String, val percent: Float, var split: Int) {
   }
   private val roundedPercent
     get() = percent.toBigDecimal().setScale(2, RoundingMode.HALF_EVEN)
-  val tip: String
+
+  val tipNum: String
+    get() = amount.toBigDecimalOrNull()?.multiply(roundedPercent)?.
+      setScale(2, RoundingMode.HALF_EVEN)?.toString() ?: ZERO_BILL
+  val tip: String get() = "$$tipNum"
+
+  val totalNum: String
     get() = amount.toBigDecimalOrNull()?.
-      multiply(roundedPercent)?.setScale(2, RoundingMode.HALF_EVEN)?.
+      add(BigDecimal(tipNum))?.setScale(2, RoundingMode.HALF_EVEN)?.
         toString() ?: ZERO_BILL
-  val total: String
-    get() = amount.toBigDecimalOrNull()?.
-      add(BigDecimal(tip))?.setScale(2, RoundingMode.HALF_EVEN)?.
-        toString() ?: ZERO_BILL
-  val perPersonTip: String
-    get() = tip.toBigDecimalOrNull()?.
+  val total: String get() = "$$totalNum"
+
+  val perPersonTipNum: String
+    get() = tipNum.toBigDecimalOrNull()?.
       divide(BigDecimal(if (split > 0) split else 1), 2, RoundingMode.HALF_EVEN)?.
         toString() ?: ZERO_BILL
-  val perPersonTotal: String
-    get() = total.toBigDecimalOrNull()?.
+  val perPersonTip: String get() = "$$perPersonTipNum"
+
+  val perPersonTotalNum: String
+    get() = totalNum.toBigDecimalOrNull()?.
       divide(BigDecimal(if (split > 0) split else 1), 2, RoundingMode.HALF_EVEN)?.
         toString() ?: ZERO_BILL
+  val perPersonTotal: String get() = "$$perPersonTotalNum"
 }
