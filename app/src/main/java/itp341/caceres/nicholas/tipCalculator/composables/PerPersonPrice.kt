d5c3f26287471fa51772afb164b5c591ff14e08a
NLCaceres <NLCaceres21@Gmail.com>
2026-04-28 20:03:35 -0700

    Convert PerPerson composable to use LabeledText with its recent changes
    - Uses LabeledText value param without the String interpolation "$" and the label
    weight + align parameters, FontWeight.Medium + TextAlign.End respectively
      - Preview highlights difference in the tip/total value usage

--- app/src/main/java/itp341/caceres/nicholas/tipCalculator/composables/PerPersonPrice.kt
+++ app/src/main/java/itp341/caceres/nicholas/tipCalculator/composables/PerPersonPrice.kt
@@ -11,20 +11,15 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import itp341.caceres.nicholas.tipCalculator.composables.reusable.LabeledText
 
 @Composable
 fun PerPerson(tip: String, total: String) {
   Row(Modifier.padding(vertical = 10.dp, horizontal = 16.dp)) {
     Text("Per Person", Modifier.weight(0.3f), fontSize = 18.sp, fontWeight = FontWeight.Bold)
     Column(Modifier.weight(0.7f).padding(start = 16.dp)) {
-      Row(Modifier.padding(bottom = 16.dp)) {
-        Text("Tip", Modifier.weight(0.3f).padding(end = 16.dp), fontSize = 18.sp, textAlign = TextAlign.End)
-        Text("$$tip", Modifier.weight(0.7f), fontSize = 18.sp)
-      }
-      Row {
-        Text("Total", Modifier.weight(0.3f).padding(end = 16.dp), fontSize = 18.sp, textAlign = TextAlign.End)
-        Text("$$total", Modifier.weight(0.7f), fontSize = 18.sp)
-      }
+      LabeledText("Tip", tip, Modifier.padding(bottom = 16.dp), FontWeight.Medium, TextAlign.End)
+      LabeledText("Total", total, labelWeight = FontWeight.Medium, labelAlign = TextAlign.End)
     }
   }
 }
@@ -32,5 +27,8 @@ fun PerPerson(tip: String, total: String) {
 @Preview(widthDp = 360, heightDp = 500, showBackground = true)
 @Composable
 fun PerPersonPreview() {
-  PerPerson("10.00", "20.00")
+  Column {
+    PerPerson("10.00", "20.00")
+    PerPerson("$10.00", "$20.00")
+  }
 }
