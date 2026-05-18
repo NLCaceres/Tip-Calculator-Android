03b9a09389ecaada59a68ba058c091e13e34c1e4
NLCaceres <NLCaceres21@Gmail.com>
2026-05-17 20:45:26 -0700

    Add `Scaffold` with `TopAppBar` to wrap the `MainScreen`
    - `MainScreen` receives the inner padding from the `Scaffold` to adjust its position down
    - Adds Preview to show how it works

--- app/src/main/java/itp341/caceres/nicholas/tipCalculator/MainActivity.kt
+++ app/src/main/java/itp341/caceres/nicholas/tipCalculator/MainActivity.kt
@@ -4,16 +4,42 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import itp341.caceres.nicholas.tipCalculator.composables.MainScreen
 
 class MainActivity : ComponentActivity() {
   private var viewModel = ViewModelMain()
 
+  @OptIn(ExperimentalMaterial3Api::class)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
     setContent {
-      MainScreen(viewModel)
+      Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+        TopAppBar(title = { Text("Tip Calculator") })
+      }) { innerPadding ->
+        MainScreen(viewModel, Modifier.padding(innerPadding))
+      }
     }
+  }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun AppPreview() {
+  val viewModel = ViewModelMain()
+  Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+    TopAppBar(title = { Text("Tip Calculator") })
+  }) { innerPadding ->
+    MainScreen(viewModel, Modifier.padding(innerPadding))
   }
 }
