c052b3c371114eee9c93f11a6ebd02cc265b5096
NLCaceres <NLCaceres21@Gmail.com>
2026-05-19 19:09:33 -0700

    Add `lifecycle-viewmodel-compose` dependency to add `viewModel` helper func in MainActivity
    - Able to drop the `viewModel` property from the Activity and simply grab the same viewModel
    instance from the ViewModelStore in the activity via CompositionLocal in `viewModel()`
    - Also dropped the extra `@OptIn` which isn't needed except for on the `MainAppBar` composable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.activity:activity-compose:1.13.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
--- app/src/main/java/itp341/caceres/nicholas/tipCalculator/MainActivity.kt
+++ app/src/main/java/itp341/caceres/nicholas/tipCalculator/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -13,18 +12,17 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import itp341.caceres.nicholas.tipCalculator.composables.MainScreen
 import itp341.caceres.nicholas.tipCalculator.composables.reusable.MainAppBar
 import itp341.caceres.nicholas.tipCalculator.composables.reusable.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
-  private val viewModel: ViewModelMain by viewModels()
-
-  @OptIn(ExperimentalMaterial3Api::class)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
     setContent {
+      val viewModel: ViewModelMain = viewModel()
       AppWithTheme(viewModel)
     }
   }
